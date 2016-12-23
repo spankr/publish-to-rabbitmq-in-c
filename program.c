65601f81f3d95203df16f9f1b3a4e7b6e344678b
Lee Vettleson <lee.vettleson@digikey.com>
2016-12-23 14:32:46 -0600

    initial commit

---
+++ program.c
@@ -0,0 +1,8 @@
+#include "program.h"
+
+
+int main(int argc, char** argv){
+
+    printf("Hello World\n");
+    return 1;
+}
---
+++ program.h
@@ -0,0 +1,9 @@
+#ifndef __PROGRAM_H
+#define __PROGRAM_H
+
+/*
+ * Important header stuff goes here.
+ */
+
+#include <stdio.h>
+#endif
