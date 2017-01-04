a4b3d4b1bab19638883df75b1197d12ae30f9141
Lee Vettleson <lee.vettleson@digikey.com>
2017-01-04 15:19:19 -0600

    nice saving point for some amqp stuff

--- program.c
+++ program.c
@@ -4,5 +4,10 @@
 int main(int argc, char** argv){
 
     printf("Hello World\n");
+
+    // We will need to open a socket.
+
+    // then probably a login
+    // localhost 5671 guest/guest
     return 1;
 }
--- program.h
+++ program.h
@@ -3,7 +3,31 @@
 
 /*
  * Important header stuff goes here.
+ * Borrowed heavily from https://github.com/alanxz/rabbitmq-c
  */
 
 #include <stdio.h>
+
+
+/**
+ * boolean type 0 = false, true otherwise
+ *
+ * \since v0.1
+ */
+typedef int amqp_boolean_t;
+
+/**
+ * Parameters used to connect to the RabbitMQ broker
+ *
+ * \since v0.2
+ */
+struct amqp_connection_info {
+  char *user;                 /**< the username to authenticate with the broker, default on most broker is 'guest' */
+  char *password;             /**< the password to authenticate with the broker, default on most brokers is 'guest' */
+  char *host;                 /**< the hostname of the broker */
+  char *vhost;                /**< the virtual host on the broker to connect to, a good default is "/" */
+  int port;                   /**< the port that the broker is listening on, default on most brokers is 5672 */
+  amqp_boolean_t ssl;
+};
+
 #endif
